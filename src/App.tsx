d749f5fc41e678f02f5ab73495c62db2fa5519eb
Allan D <afdeltour@gmail.com>
2023-10-23 15:23:45 +0200

    - Tailwind added - First page (Room) with Tailwind WIP

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+export const App =  () => {
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="Room w-screen h-screen bg-zinc-500">
+      <img className="BlindTestLogoStock w-[40%] left-[35%] right-[50%] top-[50px] relative" src="./src/assets/blindtest.png" />
+      <div className="CountdownSection w-[600px] h-[288px] left-[40%] top-[400px] absolute">
+          <div className="ImagePlaceholder w-full h-full  bg-zinc-300" />
+          <div className="Countdown w-full top-[130px] flex justify-center items-center absolute  text-xl">Timer</div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <div className="AnswerSection w-80 h-20 left-[50%] top-[750px] absolute">
+        <div className="Artist w-20 h-6 absolute text-xl">Artiste</div>
+        <div className="ArtistAnswer w-60 h-6 left-[100px] absolute text-black text-opacity-50 text-xl ">Réponse ...</div>
+        <div className="Title w-20 h-6 top-[63px] text-xl absolute">Titre</div>
+        <div className="AnswerTitle w-60 h-6 left-[100px] top-[63px] absolute text-black text-opacity-50 text-xl">Réponse ...</div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      <div className="ScoreboardSection left-[20px] top-[325px] absolute">
+        <div className="Background w-[160px] h-[320px] bg-zinc-300" />
+        <div className="Score w-40 top-[5px] absolute text-center text-xl ">Score</div>
+        <div className="LineSeparator w-[160px] top-[34px] absolute border border-black"></div>
+        <div className="Player1 w-24 left-[5px] top-[43px] absolute text-xl ">Player 1</div>
+        <div className="Player2 w-24 left-[5px] top-[71px] absolute text-xl ">Player 2</div>
+      </div>
+    </div>
   )
 }
-
-export default App
