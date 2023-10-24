254da70f58b67be47c63a20e5d5efdf87269ee93
Allan D <afdeltour@gmail.com>
2023-10-24 18:16:22 +0200

    - Remade Homepage with mapping - Some changes to Room - Added Navigate (Room & Accueil)

--- src/components/homepage/homepage.tsx
+++ src/components/homepage/homepage.tsx
@@ -1,120 +1,26 @@
+import { useNavigate } from "react-router-dom"
 export const Homepage = () => {
+const mostPopular = ["Rock", "Pop", "Electro", "Rap", "K-Pop", "Animes", "Eurodance", "Classiques"]
+const navigate = useNavigate()
+
     return (
-    <div className="Homepage">
-        <img className="BlindTestLogoStock w-[40%] mx-[35%] my-10 relative" src="./src/assets/blindtest.png" />
-        <div className="Category1 w-screen mx-2 mb-5 relative">
-        <div className="MostPopular text-xl">Most Popular</div>
-            <div className="Rectangle1 w-[98%] h-[10%] border border-black flex">
-                <div className="SongCategory mx-5 my-2 w-[10%]">
-                    <img className="RockLogo" src="./src/assets/logo_ph.jpg" />
-                    <div className="Rock text-center">Rock</div>
-                </div>
-                <div className="SongCategory mx-5 my-2 w-[10%]">
-                    <img className="PopLogo" src="./src/assets/logo_ph.jpg" />
-                    <div className="Pop text-center">Pop</div>
-                </div>
-                <div className="SongCategory mx-5 my-2 w-[10%]">
-                    <img className="ElectroLogo" src="./src/assets/logo_ph.jpg" />
-                    <div className="Electro text-center">Electro</div>
-                </div>
-                <div className="SongCategory mx-5 my-2 w-[10%]">
-                    <img className="RapLogo" src="./src/assets/logo_ph.jpg" />
-                    <div className="Rap text-center">Rap</div>
-                </div>
-                <div className="SongCategory mx-5 my-2 w-[10%]">
-                    <img className="KPopLogo" src="./src/assets/logo_ph.jpg" />
-                    <div className="KPop text-center">K-Pop</div>
-                </div>
-                <div className="SongCategory mx-5 my-2 w-[10%]">
-                    <img className="AnimeLogo" src="./src/assets/logo_ph.jpg" />
-                    <div className="Animes text-center">Animes</div>
-                </div>
-                <div className="SongCategory mx-5 my-2 w-[10%]">
-                    <img className="EurodanceLogo" src="./src/assets/logo_ph.jpg" />
-                    <div className="Eurodance text-center">Eurodance</div>
-                </div>
-                <div className="SongCategory mx-5 my-2 w-[10%]">
-                    <img className="Classiques" src="./src/assets/logo_ph.jpg" />
-                    <div className="Classiques text-center">Classiques</div>
+        <div className="Homepage">
+            <img className="BlindTestLogoStock w-[40%] mx-[35%] my-10 relative" src="./src/assets/blindtest.png" onClick={() => navigate(`/room`)}/>
+            <div className="Category1 w-screen mx-2 mb-5 relative">
+                <div className="MostPopular text-xl">Most Popular</div>
+                <div className="Rectangle1 w-[98%] h-[10%] border border-black flex">
+                    {mostPopular.map((category, index) => (
+                        <div className="SongCategory mx-5 my-2 w-[10%]"
+                            key={index}
+                            onClick={() =>
+                                console.log("You clicked on " + category)
+                            }>
+                            <img className={`${category}Logo`} src="./src/assets/logo_ph.jpg" />
+                            <div className={`${category} text-center`}>{category}</div>
+                        </div>
+                    ))}
                 </div>
             </div>
-        </div>
-
-        <div className="Category2 w-screen ml-2 mb-5 relative">
-        <div className="MostPopular text-xl">Etrangères</div>
-            <div className="Rectangle1 w-[98%] h-[10%] border border-black flex">
-                <div className="SongCategory mx-5 my-2 w-[10%]">
-                    <img className="RockLogo" src="./src/assets/logo_ph.jpg" />
-                    <div className="Rock text-center">Rock</div>
-                </div>
-                <div className="SongCategory mx-5 my-2 w-[10%]">
-                    <img className="PopLogo" src="./src/assets/logo_ph.jpg" />
-                    <div className="Pop text-center">Pop</div>
-                </div>
-                <div className="SongCategory mx-5 my-2 w-[10%]">
-                    <img className="ElectroLogo" src="./src/assets/logo_ph.jpg" />
-                    <div className="Electro text-center">Electro</div>
-                </div>
-                <div className="SongCategory mx-5 my-2 w-[10%]">
-                    <img className="RapLogo" src="./src/assets/logo_ph.jpg" />
-                    <div className="Rap text-center">Rap</div>
-                </div>
-                <div className="SongCategory mx-5 my-2 w-[10%]">
-                    <img className="KPopLogo" src="./src/assets/logo_ph.jpg" />
-                    <div className="KPop text-center">K-Pop</div>
-                </div>
-                <div className="SongCategory mx-5 my-2 w-[10%]">
-                    <img className="AnimeLogo" src="./src/assets/logo_ph.jpg" />
-                    <div className="Animes text-center">Animes</div>
-                </div>
-                <div className="SongCategory mx-5 my-2 w-[10%]">
-                    <img className="EurodanceLogo" src="./src/assets/logo_ph.jpg" />
-                    <div className="Eurodance text-center">Eurodance</div>
-                </div>
-                <div className="SongCategory mx-5 my-2 w-[10%]">
-                    <img className="Classiques" src="./src/assets/logo_ph.jpg" />
-                    <div className="Classiques text-center">Classiques</div>
-                </div>
-            </div>
-        </div>
-
-        <div className="Category3 w-screen ml-2 mb-5 relative">
-        <div className="MostPopular text-xl">Coréennes</div>
-            <div className="Rectangle1 w-[98%] h-[10%] border border-black flex">
-                <div className="SongCategory mx-5 my-2 w-[10%]">
-                    <img className="RockLogo" src="./src/assets/logo_ph.jpg" />
-                    <div className="Rock text-center">Rock</div>
-                </div>
-                <div className="SongCategory mx-5 my-2 w-[10%]">
-                    <img className="PopLogo" src="./src/assets/logo_ph.jpg" />
-                    <div className="Pop text-center">Pop</div>
-                </div>
-                <div className="SongCategory mx-5 my-2 w-[10%]">
-                    <img className="ElectroLogo" src="./src/assets/logo_ph.jpg" />
-                    <div className="Electro text-center">Electro</div>
-                </div>
-                <div className="SongCategory mx-5 my-2 w-[10%]">
-                    <img className="RapLogo" src="./src/assets/logo_ph.jpg" />
-                    <div className="Rap text-center">Rap</div>
-                </div>
-                <div className="SongCategory mx-5 my-2 w-[10%]">
-                    <img className="KPopLogo" src="./src/assets/logo_ph.jpg" />
-                    <div className="KPop text-center">K-Pop</div>
-                </div>
-                <div className="SongCategory mx-5 my-2 w-[10%]">
-                    <img className="AnimeLogo" src="./src/assets/logo_ph.jpg" />
-                    <div className="Animes text-center">Animes</div>
-                </div>
-                <div className="SongCategory mx-5 my-2 w-[10%]">
-                    <img className="EurodanceLogo" src="./src/assets/logo_ph.jpg" />
-                    <div className="Eurodance text-center">Eurodance</div>
-                </div>
-                <div className="SongCategory mx-5 my-2 w-[10%]">
-                    <img className="Classiques" src="./src/assets/logo_ph.jpg" />
-                    <div className="Classiques text-center">Classiques</div>
-                </div>
-            </div>
-        </div>
     </div>
     )
 }
--- src/components/room/room.tsx
+++ src/components/room/room.tsx
@@ -1,17 +1,18 @@
-
+import { useNavigate } from "react-router-dom"
 export const Room = () => {
+  const navigate = useNavigate()
   return (
       <div className="Room">
-        <img className="BlindTestLogoStock w-[40%] mx-[35%] my-10 relative" src="./src/assets/blindtest.png" />
+        <img className="BlindTestLogoStock w-[40%] mx-[35%] my-10 relative" src="./src/assets/blindtest.png" onClick={() => navigate(`/`)}/>
         <div className="CountdownSection w-[40%] h-[288px] mx-[35%] mb-10 relative bg-zinc-300">
             <div className="ImagePlaceholder w-full h-full bg-zinc-300" />
             <div className="Countdown w-full top-[130px] flex justify-center items-center absolute  bg-zinc-300 text-xl">Timer</div>
         </div>
-        <div className="AnswerSection mx-[50%] relative">
-          <div className="Artist w-20 h-6 absolute text-xl ">Artiste</div>
-          <div className="ArtistAnswer w-60 h-6 left-[100px] absolute text-black text-opacity-50 text-xl ">Réponse ...</div>
-          <div className="Title w-20 h-6 top-[63px] text-xl absolute">Titre</div>
-          <div className="AnswerTitle w-60 h-6 left-[100px] top-[63px] absolute text-black text-opacity-50 text-xl">Réponse ...</div>
+        <div className="AnswerSection mx-[45%] relative">
+          <div className="Artist w-20 h-10 absolute text-xl border-2 border-black rounded-lg text-center align-middle	">Artiste</div>
+          <div className="ArtistAnswer w-60 h-10 left-[100px]  absolute border-2 border-black rounded-lg  text-black text-opacity-50 text-xl ">Réponse ...</div>
+          <div className="Title w-20 h-10 top-[63px] text-xl border-2 border-black rounded-lg absolute text-center">Titre</div>
+          <div className="AnswerTitle w-60 h-10 left-[100px] top-[63px] absolute border-2  border-black rounded-lg text-black text-opacity-50 text-xl">Réponse ...</div>
         </div>
         <div className="ScoreboardSection ml-5 top-[325px] absolute">
           <div className="Background w-[150px] h-[300px] bg-zinc-300" />
